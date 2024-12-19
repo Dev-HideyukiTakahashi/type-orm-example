d90fe1506aecc5f1701b8acda15a018a0cf73fc5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-19 18:16:12 -0300

    crud entity manager

--- MyProject/src/index.ts
+++ MyProject/src/index.ts
@@ -1,46 +1,7 @@
 import { AppDataSource } from "./data-source"
+import { criarUsuario, listarTodosUsuarios, listarTodosVeiculos, removerUsuarioPeloNome, salvarVeiculo } from "./entity-manager"
 import { User } from "./entity/User"
 import { Vehicle } from "./entity/Vehicle"
-
-async function criarUsuario(user: User) {
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-}
-
-async function salvarVeiculo(vehicle: Vehicle) {
-    await AppDataSource.manager.save(vehicle)
-    console.log("Vehicle a new user with id: " + vehicle.id)
-}
-
-async function listarTodosUsuarios() {
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-    return users;
-}
-
-async function listarTodosVeiculos() {
-    console.log("Loading vehicles from the database...")
-    const vehicles = await AppDataSource.manager.find(Vehicle)
-    console.log("Loaded vehicles: ", vehicles)
-    return vehicles;
-}
-
-async function removerUsuario(user: User) {
-    console.log("Deleting user from the database...")
-    const users = await AppDataSource.manager.delete(User, user.id)
-    console.log("User deleted: ", user)
-}
-
-async function removerUsuarioPeloNome(name: string) {
-    console.log("Deleting user from the database with name... " + name)
-    const usr = await AppDataSource.manager.findOneBy(User, {
-        firstName: name
-    });
-    if (usr != null && usr.id > 0) {
-        await removerUsuario(usr)
-    }
-}
 
 AppDataSource.initialize().then(async () => {
 
